f3ca7e26ce30c385262e321c1340a2fd3824fbe6
Gustavo da Costa Fontana <162226403+gucfontana@users.noreply.github.com>
2024-07-30 14:23:41 -0300

    Add project files.

---
+++ ControleDeCinema/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ControleDeCinema.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
