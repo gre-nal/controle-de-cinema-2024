c1b1475f4c7fc183e1e209e90e63786926764c3b
Gustavo da Costa Fontana <162226403+gucfontana@users.noreply.github.com>
2024-07-31 16:03:12 -0300

    att salas

---
+++ ControleDeCinema/Controllers/SalasController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeCinema.Controllers
+{
+    public class SalasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Apagar_Confirmacao()
+        {
+            return View();
+        }
+    }
+}
---
+++ ControleDeCinema/Views/Salas/Criar.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ControleDeCinema.Views.Sala
+{
+    public class Criar : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
