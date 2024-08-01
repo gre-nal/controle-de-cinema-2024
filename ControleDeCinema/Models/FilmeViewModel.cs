011731e36949fb5c48bc539024d9253d2d0cd7ad
Matheus-Branco <jeremiasmatheus007@gmail.com>
2024-08-01 18:17:18 -0300

    ViewModel filme e sessao

---
+++ ControleDeCinema/Models/FilmeViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeCinema.Models
+{
+    public class CadastrarFilmeViewModel
+    {
+        [Required(ErrorMessage = "É necessário preencher o título!")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "É necessário selecionar um gênero!")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "É necessário preencher com a duracao!")]
+        public TimeOnly Duracao { get; set; }
+
+        [Required(ErrorMessage = "É necessário preencher a descrição!")]
+        public string Descricao { get; set; }
+    }
+
+    public class ApagarFilmeViewModel
+    {
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public TimeOnly Duracao { get; set; }
+        public string Descricao { get; set; }
+    }
+
+    public class ListarFilmeViewModel
+    {
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public TimeOnly Duracao { get; set; }
+    }
+
+    public class DetalhesFilmeViewModel
+    {
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public TimeOnly Duracao { get; set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ ControleDeCinema/Models/SessoesViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeCinema.Models
+{
+    public class CadastrarSessaoViewModel
+    {
+        [Required(ErrorMessage = "É necessário selecionar uma sala!")]
+        [Range(1, int.MaxValue, ErrorMessage = "É necessário que a sala seja um número válido!")]
+        public int NumeroSala { get; set; }
+
+        [Required(ErrorMessage = "É necessário selecionar um filme!")]
+        public string TituloFilme { get; set; }
+
+        [Required(ErrorMessage = "É necessário preencher o horário!")]
+        public TimeOnly Horario { get; set; }
+
+        [Required(ErrorMessage = "É necessário preencher a quantidade de ingressos!")]
+        public string QuatidadeIngresso { get; set; }
+
+        [Required(ErrorMessage = "É necessário preencher a cadacidade da sala!")]
+        public int CapacidadeSala { get; set; }
+
+        public IEnumerable<SelectListItem>? Filmes { get; set; }
+        public IEnumerable<SelectListItem>? Salas { get; set; }
+    }
+
+    public class ApagarSessaoViewModel
+    {
+        public string Filme { get; set; }
+        public TimeOnly Horario { get; set; }
+        public string Descricao { get; set; }
+        public string QuatidadeIngresso { get; set; }
+    }
+
+    public class ListarSessaoViewModel
+    {
+        public string TituloFilme { get; set; }
+
+        public TimeOnly Horario { get; set; }
+    }
+
+    public class DetalhesSessaoViewModel
+    {
+        public int NumeroSala { get; set; }
+
+        public string TituloFilme { get; set; }
+
+        public TimeOnly Horario { get; set; }
+
+        public string QuatidadeIngresso { get; set; }
+
+        public int CapacidadeSala { get; set; }
+    }
+}
