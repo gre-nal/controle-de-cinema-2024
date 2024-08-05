51037e28ea086a0de0ab24d3ad51800e6e8ed223
Matheus-Branco <jeremiasmatheus007@gmail.com>
2024-08-05 15:15:45 -0300

    ViewModel "pronto"

---
+++ ControleDeCinema/Models/FuncionariosViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace ControleDeCinema.Models
+{
+    public class ComplexidadeDaSenha : ValidationAttribute
+    {
+        private readonly int _minLength;
+
+        public ComplexidadeDaSenha(int minLength)
+        {
+            _minLength = minLength;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var password = value as string;
+            if (password == null || password.Length < _minLength)
+            {
+                return new ValidationResult($"A senha deve ter pelo menos {_minLength} caracteres.");
+            }
+
+            if (!Regex.IsMatch(password, @"[A-Z]"))
+            {
+                return new ValidationResult("A senha deve conter pelo menos uma letra maiúscula.");
+            }
+
+            if (!Regex.IsMatch(password, @"[a-z]"))
+            {
+                return new ValidationResult("A senha deve conter pelo menos uma letra minúscula.");
+            }
+
+            if (!Regex.IsMatch(password, @"[0-9]"))
+            {
+                return new ValidationResult("A senha deve conter pelo menos um número.");
+            }
+
+            if (!Regex.IsMatch(password, @"[\W_]"))
+            {
+                return new ValidationResult("A senha deve conter pelo menos um caractere especial.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+
+    public class CadastrarFuncionariosViewModel
+    {
+        [Required(ErrorMessage = "É necessário preencher o nome!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "É necessário preencher o usuário!")]
+        public int Login { get; set; }
+
+        [Required(ErrorMessage = "É necessário preencher a senha!")]
+        [ComplexidadeDaSenha(8, ErrorMessage = "A senha deve ter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas e caracteres especiais.")]
+        public string Senha { get; set; }
+
+    }
+
+    public class ApagarFuncionariosViewModel
+    {
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+
+    public class ListarFuncionariosViewModel
+    {
+        public string login { get; set; }
+    }
+
+    public class DetalhesFuncionariosViewModel
+    {
+        public int Nome { get; set; }
+        public int login { get; set; }
+    }
+}
---
+++ ControleDeCinema/Models/SalasViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeCinema.Models
+{
+    public class CadastrarSalasViewModel
+    {
+        [Required(ErrorMessage = "É necessário preencher um número uma sala!")]
+        [Range(1, int.MaxValue, ErrorMessage = "É necessário que a sala seja um número válido!")]
+        public int Numero { get; set; }
+
+        [Required(ErrorMessage = "É necessário preencher a capacidade!")]
+        [Range(1, int.MaxValue, ErrorMessage = "É necessário que a capacidade tenha um número válido!")]
+        public int Capacidade { get; set; }
+    }
+
+    public class ApagarSalasViewModel
+    {
+        public int Numero { get; set; }
+        public int Capacidade { get; set; }
+    }
+
+    public class ListarSalasViewModel
+    {
+        public int Numero { get; set; }
+    }
+
+    public class DetalhesSalasViewModel
+    {
+        public int Numero { get; set; }
+        public int Capacidade { get; set; }
+    }
+}
