c8187a00a66a87994c9f760076d37ca46aee4bd2
Gustavo da Costa Fontana <162226403+gucfontana@users.noreply.github.com>
2024-08-09 15:30:20 -0300

    add. modelo de camadas

---
+++ ControleDeCinema.Data/Compartilhado/RepositorioBaseEmOrm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeCinema.Data.Compartilhado
+{
+    internal class Class1
+    {
+    }
+}
